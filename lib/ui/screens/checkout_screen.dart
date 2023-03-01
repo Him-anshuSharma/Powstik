25e89d5e7e2bf22dc3fc0767af6b2f663f65d48e
Himanshu Sharma <sharmanshu0103@gmail.com>
2023-03-01 21:19:30 +0530

    feat: added discount option and screenshots

--- lib/ui/screens/checkout_screen.dart
+++ lib/ui/screens/checkout_screen.dart
@@ -3,10 +3,17 @@ import 'package:provider/provider.dart';
 import 'package:powstik/ui/theme/theme_constants.dart' as theme;
 import '../../provider/cart.dart';
 
-
-class CheckoutScreen extends StatelessWidget {
+class CheckoutScreen extends StatefulWidget {
   static String checkoutScreen = 'CheckoutScreen';
   const CheckoutScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CheckoutScreen> createState() => _CheckoutScreenState();
+}
+
+class _CheckoutScreenState extends State<CheckoutScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _discountCode = '';
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +29,23 @@ class CheckoutScreen extends StatelessWidget {
           Container(
             color: theme.COLOR_PRIMARY,
             child: Padding(
-              padding: const EdgeInsets.fromLTRB(2,12,12,12),
+              padding: const EdgeInsets.fromLTRB(2, 12, 12, 12),
               child: Row(
                 children: [
-                  Image.asset("assets/images/logo.png",height: 30,),
-                  SizedBox(width: 10,),
-                  Text("basket",style: TextStyle(fontSize: 28,color: Colors.white,fontWeight: FontWeight.bold),),
+                  Image.asset(
+                    "assets/images/logo.png",
+                    height: 30,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "basket",
+                    style: TextStyle(
+                        fontSize: 28,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ],
               ),
             ),
@@ -42,12 +60,41 @@ class CheckoutScreen extends StatelessWidget {
                     ListTile(
                       leading: Image.network(item.itemImage),
                       title: Text("${item.itemQty} x ${item.itemName}"),
-                      subtitle: Text('\$${item.itemPrice.toStringAsFixed(2)}'),
+                      subtitle:
+                      Text('\$${item.itemPrice.toStringAsFixed(2)}'),
                     ),
-                    const Divider(thickness: 3,)
+                    const Divider(
+                      thickness: 3,
+                    )
                   ],
                 );
               },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Enter Discount Code:',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  TextFormField(
+                    onChanged: (value) {
+                      _discountCode = value;
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a discount code';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
           Text('Total: \$${cart.totalAmount.toStringAsFixed(2)}'),
@@ -57,7 +104,11 @@ class CheckoutScreen extends StatelessWidget {
               onPrimary: Colors.white,
             ),
             onPressed: () {
-              // place order
+              if (_formKey.currentState!.validate()) {
+                // Apply discount code
+
+                // Place order
+              }
             },
             child: const Text('Place Order'),
           ),
